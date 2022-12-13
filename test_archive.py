e304b71204a936924343f79159f9d0ed2391800f
Rustam Dzhafarov <rustamd74@gmail.com>
2022-12-13 23:00:38 +0300

    проба

--- test_archive.py
+++ test_archive.py
@@ -4,6 +4,7 @@ import os
 import pytest
 import xlrd
 from PyPDF2 import PdfReader
+from os.path import basename
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 path_resources = os.path.join(current_dir, 'resources')
@@ -12,22 +13,21 @@ path_myzip = os.path.join(path_resources, 'archive.zip')
 
 # Архив
 
-def test_create_archive(myzip=None):
+def test_create_archive():
     with ZipFile(os.path.join(path_resources, 'archive.zip'), 'w', compression=ZIP_DEFLATED) as myzip:
-        myzip.write(os.path.join(path_resources, 'names.csv'))
-        myzip.write(os.path.join(path_resources, 'docs-pytest-org-en-latest.pdf'))
-        myzip.write(os.path.join(path_resources, 'accounts.xlsx'))
+        myzip.write(os.path.join(path_resources, 'names.csv'), 'names.csv')
+        myzip.write(os.path.join(path_resources, 'docs-pytest-org-en-latest.pdf'), 'docs-pytest-org-en-latest.pdf')
+        myzip.write(os.path.join(path_resources, 'accounts.xlsx'), 'accounts.xlsx')
 
 
 def test_csv():
-    with ZipFile(path_myzip) as zipfile:
-        csvfile = zipfile.read('names.csv')
-        with open(csvfile) as csvfile:
-            csvfile = csv.reader(csvfile)
-
-        assert csvfile.contains('Anna')
+    with ZipFile(path_myzip, 'r') as zip_file:
+            csvfile = str(zip_file.read('names.csv'))
+            with open(csvfile) as csvfile:
+                csvfile = csv.reader(csvfile)
+                assert csvfile.__contains__("Anna")
 
 # def test_pdf():
-#   pass
+#  pass
 
 # def test_xls():
