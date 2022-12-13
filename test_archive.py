c970fc09a7a77925aa78098758b94d3e25de1c5f
Rustam Dzhafarov <rustamd74@gmail.com>
2022-12-14 00:05:20 +0300

    homework 6

--- test_archive.py
+++ test_archive.py
@@ -1,10 +1,7 @@
-import csv
 from zipfile import ZipFile, ZIP_DEFLATED
 import os
-import pytest
-import xlrd
 from PyPDF2 import PdfReader
-from os.path import basename
+from openpyxl.reader.excel import load_workbook
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 path_resources = os.path.join(current_dir, 'resources')
@@ -20,14 +17,28 @@ def test_create_archive():
         myzip.write(os.path.join(path_resources, 'accounts.xlsx'), 'accounts.xlsx')
 
 
+# Проверка csv-файла
 def test_csv():
     with ZipFile(path_myzip, 'r') as zip_file:
-            csvfile = str(zip_file.read('names.csv'))
-            with open(csvfile) as csvfile:
-                csvfile = csv.reader(csvfile)
-                assert csvfile.__contains__("Anna")
+        csvfile = str(zip_file.read('names.csv'))
+        assert csvfile.__contains__("Anna")
 
-# def test_pdf():
-#  pass
+# Проверка pdf-файла
+def test_pdf():
+    with ZipFile(path_myzip, 'r') as zip_pdf_file:
+        pdf_file = zip_pdf_file.extract('docs-pytest-org-en-latest.pdf')
+        reader = PdfReader(pdf_file)
+        page = reader.pages[0]
+        text = page.extractText()
+        assert 'Release 0.1' in text
+        os.remove(pdf_file)
 
-# def test_xls():
+# Проверка xlsx-файла
+def test_xls():
+    with ZipFile(path_myzip, 'r') as zip_xlsx_file:
+        xlsx_file = zip_xlsx_file.extract('accounts.xlsx')
+        xlsxfile = load_workbook(xlsx_file)
+        sheet = xlsxfile.active
+        assert sheet.cell(row=3, column=3).value == 'Smith'
+        os.remove(xlsx_file)
+
